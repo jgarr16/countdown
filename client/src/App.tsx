22aa969b6ebea2211b9f9d7fb4875483106dec00
jgarr16 <15323251+jgarr16@users.noreply.github.com>
2026-01-01 23:30:45 -0800

    Set wouter base for Pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
 import { Switch, Route } from "wouter";
+import { Router as WouterRouter } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -21,9 +22,9 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
         <Toaster />
-        <BrowserRouter basename="/countdown">
+        <WouterRouter base="/countdown">
           <Router />
-        </BrowserRouter>
+        </WouterRouter>
       </TooltipProvider>
     </QueryClientProvider>
   );
